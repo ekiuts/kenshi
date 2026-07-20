70ac7b88541649e187eac678ba504df997fedea7
farghul <byron.stuike@gov.bc.ca>
2026-07-19 20:15:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Kenshi Compact
+
+![Banner](misc/banner.png)
+
+It's an apocalyptic world where you can't climb an inch on Maslow's Heirchy of Needs. You're starving, penniless, and constantly being chased by bandits. In a world like that the solution is clear... more vanity.
+
+Have you ever been crawling your way from Bast to Stoat, because a pack of Skimmers didn't like the way you interupted their patrol? And while dragging the ravaged tendrils of your once beautiful legs, hoping against hope that the bartender in Stoat will have a `First Aid Kit` for sale, you think to yourself *I hate my cheekbones*? Then Kenshi-Compact is for you.
+
+I love Kenshi, but as much as I enjoy playing the game, I love adjusting my character's look... constantly. So I created this mod in order to access the Character Editor anytime. Thought I'd share it in case anyone else is obsessed with sliders.
+
+This mod is very simple. It opens the Character Editor anytime during gameplay, with the exception of during active combat. There are no gameplay enhancements or modifications, you just get to play the `one more change` game with any of your active characters.
+
+It's currently set to listen for the `ALT+V` key.
+
+![Reflection](misc/reflection.png)
+
+## Future Plans
+
+I hope to eventually let the `key binding` be a player decision.
---
+++ src/Kenshi-Compact.cpp
@@ -0,0 +1,99 @@
+#include <Debug.h>
+#include <Globals.h>
+#include <GameWorld.h>
+#include <PlayerInterface.h>
+#include <Character.h>
+#include <Windows.h>
+
+static bool g_openEditor = false;
+static bool g_timerStarted = false;
+
+static void CALLBACK TimerProc(HWND, UINT, UINT_PTR, DWORD);
+
+static bool isKenshiForeground()
+{
+    HWND fg = GetForegroundWindow();
+    if (!fg) return false;
+
+    DWORD pid = 0;
+    GetWindowThreadProcessId(fg, &pid);
+    return pid == GetCurrentProcessId();
+}
+
+static DWORD WINAPI PollThread(LPVOID)
+{
+    bool wasDown = false;
+
+    while (true)
+    {
+        Sleep(50);
+        if (!isKenshiForeground()) continue;
+
+        bool alt = (GetAsyncKeyState(VK_MENU) & 0x8000) != 0;
+        bool v = (GetAsyncKeyState('V') & 0x8000) != 0;
+        bool down = alt && v;
+
+        if (down && !wasDown)
+        {
+            g_openEditor = true;
+
+            if (!g_timerStarted)
+            {
+                HWND hwnd = GetForegroundWindow();
+                if (hwnd)
+                {
+                    UINT_PTR result = SetTimer(hwnd, 1, 50, TimerProc);
+                    if (result)
+                    {
+                        g_timerStarted = true;
+                        DebugLog("Timer started on game HWND");
+                    }
+                    else
+                    {
+                        DebugLog("SetTimer failed");
+                    }
+                }
+            }
+        }
+
+        wasDown = down;
+    }
+}
+
+static void CALLBACK TimerProc(HWND, UINT, UINT_PTR, DWORD)
+{
+    if (!g_openEditor)
+        return;
+    g_openEditor = false;
+
+    GameWorld* gameWorld = ou;
+    if (!gameWorld || !gameWorld->player)
+        return;
+
+    if (!gameWorld->player->selectedCharacter)
+        return;
+
+    Character* character = gameWorld->player->selectedCharacter.getCharacter();
+    if (!character)
+    {
+        DebugLog("No Character Selected");
+        return;
+    }
+
+    if (character->isInCombatMode(true, true))
+    {
+        DebugLog("Character in Combat");
+        return;
+    }
+
+    DebugLog("Opening Character Editor");
+    ou->player->activateCharacterEditMode(character);
+}
+
+__declspec(dllexport) void startPlugin()
+{
+    HANDLE h = CreateThread(nullptr, 0, PollThread, nullptr, 0, nullptr);
+    if (h) CloseHandle(h);
+
+    DebugLog("Kenshi-Compact loaded");
+}
